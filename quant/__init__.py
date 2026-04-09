9fb20804783f0c72292c39a5315dbb4687b34a66
xie-tj <1875169310@qq.com>
2026-04-09 12:16:04 +0800

    fix: remove unused tdx_renko import

--- quant/__init__.py
+++ quant/__init__.py
@@ -1,8 +1,6 @@
 # 量化因子模块
 from .calc_factors import FactorCalculator
-from .tdx_renko import TDXRenkoCalculator
 
 __all__ = [
-    'FactorCalculator',
-    'TDXRenkoCalculator'
+    'FactorCalculator'
 ]
